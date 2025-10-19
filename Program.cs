7e2498cfed31739b5c047683e59b3dd26d566081
Fabis-li <fabiano_slima@hotmail.com>
2025-10-19 15:09:06 -0300

    feat: testes com as classes Nokia e Iphone

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia:");
+Nokia nokia = new Nokia(numero:"123456789", modelo:"Nokia 3310", imei:"IMEI123456", memoria:16);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.Write("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Iphone iphone = new Iphone(numero:"987654321", modelo:"iPhone 13", imei:"IMEI987654", memoria:128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
+
